9aee90c9bbe5ed339e914bb4c647a34dbd7e9055
aoqia <aoqia@aoqia.dev>
2025-07-21 18:35:48 +1000

    feat: Upgrade to Gradle 8.14.2 and Kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+val name: String by settings
+rootProject.name = name
+
+pluginManagement {
+	repositories {
+		maven {
+			name = "Fabric"
+			url = uri("https://maven.fabricmc.net/")
+		}
+		mavenCentral()
+		gradlePluginPortal()
+	}
+}
